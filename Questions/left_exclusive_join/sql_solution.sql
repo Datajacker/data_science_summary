1b3267b8ac5471e96250ad7ba358618c7270043f
Jiaqiang Yi <jiaqiang@fusionanalytics.com>
2022-05-06 11:36:54 -0400

    add alternative solution for left exclusive join

--- Questions/left_exclusive_join/sql_solution.sql
+++ Questions/left_exclusive_join/sql_solution.sql
@@ -1,11 +1,15 @@
-# this apply when we have unique key
+-- this apply when we have unique key
 SELECT <column_list>
 FROM TABLEA a
 LEFTJOIN TABLEB b 
 ON a.Key = b.Key 
 WHERE b.Key IS NULL;
 
-# without unique key
+-- NOT EXISTS
+SELECT name FROM table2 as t2
+WHERE NOT EXISTS (SELECT * FROM table1 as t1 WHERE t1.name = t2.name)
+
+--without unique key
 select id, name from table1
 minus
 select id, name from table2
