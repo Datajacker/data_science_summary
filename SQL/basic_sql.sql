368361456979d03992685d05c6accf2bd1e4ac4c
Jiaqiang Yi <jiaqiang@fusionanalytics.com>
2022-05-14 11:57:29 -0400

    add inner join

--- SQL/basic_sql.sql
+++ SQL/basic_sql.sql
@@ -29,3 +29,8 @@ BEGIN
       
   );
 END
+
+-- 181. Employees Earning More Than Their Managers
+select t1.name as Employee
+from Employee t1, Employee t2
+where t1.managerId = t2.id and t1.salary > t2.salary
