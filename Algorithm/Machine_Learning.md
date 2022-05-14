d6c716083b041fcb4e7d3ef47133a24291d97f03
Jiaqiang Yi <jiaqiang@fusionanalytics.com>
2022-05-14 00:14:30 -0400

    update md

---
+++ Algorithm/Machine_Learning.md
@@ -0,0 +1,7 @@
+* [Decision Tree](https://towardsdatascience.com/decision-tree-classifier-explained-in-real-life-picking-a-vacation-destination-6226b2b60575)
+
+* [Random Forest](https://towardsdatascience.com/random-forests-algorithm-explained-with-a-real-life-example-and-some-python-code-affbfa5a942c)
+
+* [SVM](https://medium.com/machine-learning-101/chapter-2-svm-support-vector-machine-theory-f0812effc72)
+
+* [R](https://github.com/ChenshuLiu/Machine-Learning-in-R)
---
+++ SQL/basic_sql.sql
@@ -0,0 +1,31 @@
+--176. Second Highest Salary
+salary as SecondHighestSalary
+from Employee
+order by salary
+limit 1 offset 1
+
+select max(salary) as SecondHighestSalary
+from Employee 
+where salary < (select max(salary) from Employee)
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
+-- 177. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN ( with a as(
+          SELECT  distinct salary, DENSE_RANK() over(order by salary desc) as 'Rank' 
+          FROM Employee)
+      
+      select salary
+      from a
+      where a.Rank = N
+      
+  );
+END
