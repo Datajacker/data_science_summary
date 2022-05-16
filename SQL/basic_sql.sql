bbb5a897a10f907c404fdc28ebcb44fa9f647973
Jiaqiang Yi <jiaqiang@fusionanalytics.com>
2022-05-16 11:54:15 -0400

    update second solution for self join

--- SQL/basic_sql.sql
+++ SQL/basic_sql.sql
@@ -34,3 +34,8 @@ END
 select t1.name as Employee
 from Employee t1, Employee t2
 where t1.managerId = t2.id and t1.salary > t2.salary
+
+select E.name as Employee from Employee E
+join Employee as M
+on E.ManagerId = M.Id
+where E.Salary > M.Salary;
