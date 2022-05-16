ce784fea410d3c64ba52ccf537dc6ab9efaee509
Datajacker <yijiaqiang11@gmail.com>
2022-05-16 15:35:09 -0600

    add department highest score sql

--- SQL/basic_sql.sql
+++ SQL/basic_sql.sql
@@ -28,10 +28,7 @@ BEGIN
       where a.Rank = N
       
   );
-<<<<<<< HEAD
-END
-=======
-END
+
 
 -- 181. Employees Earning More Than Their Managers
 select t1.name as Employee
@@ -42,4 +39,58 @@ select E.name as Employee from Employee E
 join Employee as M
 on E.ManagerId = M.Id
 where E.Salary > M.Salary;
->>>>>>> bbb5a897a10f907c404fdc28ebcb44fa9f647973
+
+-- 184. Department Highest Salary
+with t as(
+   select 
+   name, 
+   departmentId,salary,
+   RANK() over(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+ from Employee
+)
+
+select a.name as Department, 
+       t.name as Employee, 
+       t.salary as Salary
+from t
+left join Department a
+on t.departmentId = a.id 
+where rnk = 1
+
+/*
+WITH ranked AS(
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary,
+        RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC) AS rnk
+    FROM Employee e 
+    JOIN Department d 
+    ON e.departmentId = d.id) 
+
+-- The highest salary is just those with rank = 1
+SELECT 
+    Department,
+    Employee,
+    Salary
+FROM ranked
+WHERE rnk = 1;
+*/
+
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+    )
+;
