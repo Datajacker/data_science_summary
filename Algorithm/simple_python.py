cdd434bd3b02064382393f3de758cc56900d9a00
Datajacker <yijiaqiang11@gmail.com>
2022-06-01 00:30:01 -0600

    9. Palindrome Number

---
+++ Algorithm/simple_python.py
@@ -0,0 +1,18 @@
+# 9. Palindrome Number]
+# Given an integer x, return true if x is palindrome integer.
+# An integer is a palindrome when it reads the same backward as forward.
+# For example, 121 is a palindrome while 123 is not.
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        for i in range(0, len(x)//2):
+            if(x[i] == x[len(x)-1-i]):
+                pass
+            else:
+                return False
+            
+        return True   
