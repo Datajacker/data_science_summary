591b0372c0566cace7782b41c41bf1738289040a
Datajacker <yijiaqiang11@gmail.com>
2022-06-07 01:45:31 -0400

    leetcode 577

--- SQL/basic_sql.sql
+++ SQL/basic_sql.sql
@@ -217,3 +217,13 @@ SELECT
 FROM Activity as a
 LEFT JOIN b 
 ON a.player_id = b.player_id
+
+-- 577. Employee Bonus
+SELECT
+    Employee.name, Bonus.bonus
+FROM
+    Employee
+        LEFT JOIN
+    Bonus ON Employee.empid = Bonus.empid
+WHERE
+    bonus < 1000 OR bonus IS NULL
