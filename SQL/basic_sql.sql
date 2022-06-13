c7a42ffab4a1b49be21b39aacba9028910b77f9c
Datajacker <yijiaqiang11@gmail.com>
2022-06-13 00:05:44 -0600

    add sql 578

--- SQL/basic_sql.sql
+++ SQL/basic_sql.sql
@@ -319,3 +319,9 @@ from Candidate
 inner join ranked
 on Candidate.id = ranked.candidateId
 
+-- 578. Get Highest Answer Rate Question
+select question_id as 'survey_log'
+from SurveyLog
+group by question_id
+order by sum(if(action = 'answer', 1, 0)) / sum(if(action = 'show', 1, 0)) desc, question_id asc
+limit 1
