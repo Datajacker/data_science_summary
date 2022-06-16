785c1a8164881a1d44b0b6e44e9c43ecc1b606a7
Datajacker <yijiaqiang11@gmail.com>
2022-06-16 00:25:33 -0600

    add leetcode two sum

--- Algorithm/simple_python.py
+++ Algorithm/simple_python.py
@@ -42,3 +42,35 @@ for i in range(0,20):
     n = random.randint(1,30)
     randomlist.append(n)
 print(randomlist)       
+
+# leetcode 1 two sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+# Approach 2: Two-pass Hash Table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+
+# Approach 3: One-pass Hash Table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
