0d0cdfa946d70ede5d41664de474a6181f6f5cee
Datajacker <yijiaqiang11@gmail.com>
2022-06-28 23:40:54 -0600

    leetcode 344

--- Algorithm/simple_python.py
+++ Algorithm/simple_python.py
@@ -288,3 +288,27 @@ class Solution:
             if nums[r]:
                 nums[l],nums[r] = nums[r],nums[l]
                 l+=1
+
+# 344. Reverse String
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) -1
+        while left < right :
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+class Solution:
+    def reverseString(self, s):
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
---
+++ Algorithm/veeva/test.py
@@ -0,0 +1,16 @@
+def palindrome(string):
+    length = len(string)//2
+
+    for i in range(length):
+        if string[i] != string[-i]:
+            return(False)
+    return(True)
+
+def find(lis, target):
+
+    for i in range(len(lis)):
+        b = target - lis[i]
+        lis_update = 
+        if b in lis_update:
+            return True
+ 
